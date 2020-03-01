521bb1a7478a0cdb05073f41e689a5fcefea5e6b
dedgar1 <jnzokwe@gmail.com>
2020-03-01 08:42:46 -0500

    chapter 01, recipe 02, added secure ield and textfield

--- Chapter01 - Using the basic SwiftUI Views and Controls/02 - Dealing With Text/FormattedText/ContentView.swift
+++ Chapter01 - Using the basic SwiftUI Views and Controls/02 - Dealing With Text/FormattedText/ContentView.swift
@@ -9,33 +9,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var password = "1234"
+    @State var someText = "initial text"
     var body: some View {
         VStack{
             Text("Hello World")
-            Text("Large Title Text")
+                .fontWeight(.medium)
+            SecureField("Enter a password", text: $password)
+                .padding()
+            Text("password entered: \(password)")
+                .italic()
+            TextField("Enter some text", text: $someText)
+                .padding()
+            Text("\(someText)")
                 .font(.largeTitle)
-            Text("Bold Text")
-                .bold()
-            Text("Changing text color")
+                .underline()
+            Text("Changing text color and make it bold")
                 .foregroundColor(Color.blue)
+                .bold()
             Text("Use kerning to change space between lines of text")
                 .kerning(7)
-            Text("Changing font weight").fontWeight(.semibold)
+            Text("Changing baseline offset")
+                .baselineOffset(100)
             Text("Strikethrough")
                 .strikethrough()
-            Text("Underline")
-                .underline()
-            Text("Italisized text")
-                .italic()
+            
             Text("This is a multiline text implemented in swiftUI. The trailing modifier was added to the text. This text also implements multiple modifiers")
+                .background(Color.yellow)
                 .multilineTextAlignment(.trailing)
                 .lineSpacing(10)
-                .background(Color.yellow)
+                
             
-    
         }
-        
-        
         
     }
 }
