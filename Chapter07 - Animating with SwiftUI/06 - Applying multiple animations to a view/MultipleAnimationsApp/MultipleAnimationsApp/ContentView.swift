4c760299fa44ab9e32954d0b43a536b5769b6036
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-03-13 19:18:51 +0000

    Chapter 7, recipe 06

---
+++ Chapter07 - Animating with SwiftUI/06 - Applying multiple animations to a view/MultipleAnimationsApp/MultipleAnimationsApp/ContentView.swift
@@ -0,0 +1,49 @@
+//
+//  ContentView.swift
+//  MultipleAnimationsApp
+//
+//  Created by giordano scalzo on 13/03/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State
+    var initialState = true
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            Rectangle()
+                .fill(initialState ? Color.blue : Color.red)
+                .cornerRadius(initialState ? 50 : 0)
+                .offset(y: initialState ? 0 : -200)
+                .rotation3DEffect(initialState ? .degrees(0)
+                    : .degrees(45),
+                                  axis: (x: 1, y: 0, z: 0))
+                .animation(.easeInOut(duration: 2))
+                .scaleEffect(initialState ? 1 : 0.8)
+                .rotationEffect(initialState ? Angle(degrees:0) : Angle(degrees:-90))
+                .animation(.linear(duration: 1))
+                .frame(width: 300, height: 200)
+            
+            Button(action: {
+                self.initialState.toggle()
+            }) {
+                Text("Animate")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.green)
+                    .cornerRadius(5)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
