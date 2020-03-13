43e539272da9aef0b8a828b4286426c41bfb8a90
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-03-13 13:55:02 +0000

    Chapter 7, recipe 04

---
+++ Chapter07 - Animating with SwiftUI/04 - Applying a delay to a view modifier animation/DelayedAnimationsApp/DelayedAnimationsApp/ContentView.swift
@@ -0,0 +1,55 @@
+//
+//  ContentView.swift
+//  DelayedAnimationsApp
+//
+//  Created by giordano scalzo on 13/03/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State
+    var change = false
+    
+    var body: some View {
+        VStack(spacing: 30) {
+            
+            Rectangle()
+                .fill(Color.blue)
+                
+                .offset(y: change ? -300 : 0)
+                .animation(Animation
+                    .easeInOut(duration: 1).delay(0))
+                
+                .scaleEffect(change ? 0.5 : 1)
+                .animation(Animation
+                    .easeInOut(duration: 1).delay(1))
+                
+                .rotation3DEffect(
+                    change ? .degrees(45) : .degrees(0),
+                            axis: (x: 1, y: 0, z: 0))
+                .animation(Animation
+                    .easeInOut(duration: 1).delay(2))
+                
+                .frame(width: 200, height: 200)
+            
+            Button(action: {
+                self.change.toggle()
+            }) {
+                Text("Animate")
+                    .fontWeight(.heavy)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(Color.green)
+                .cornerRadius(5)
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
