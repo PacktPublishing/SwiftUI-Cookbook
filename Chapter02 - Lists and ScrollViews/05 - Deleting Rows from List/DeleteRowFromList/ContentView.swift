a3b29354c5204a2333a966be926a1666e7f99ef0
dedgar1 <jnzokwe@gmail.com>
2020-03-20 04:51:23 -0400

    Chapter 2, recipe 5

---
+++ Chapter02 - Lists and ScrollViews/05 - Deleting Rows from List/DeleteRowFromList/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  DeleteRowFromList
+//
+//  Created by Edgar Nzokwe on 3/20/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var countries = ["USA", "Canada", "England","Cameroon", "South Africa", "Mexico" , "Japan", "South Korea"]
+    var body: some View {
+        NavigationView{
+            List {
+                ForEach(countries, id: \.self) { country in
+                    Text(country)
+                }
+                .onDelete(perform: self.deleteItem)
+            }
+            .navigationBarTitle("Countries", displayMode: .inline)
+        }
+    }
+    private func deleteItem(at indexSet: IndexSet){
+        self.countries.remove(atOffsets: indexSet)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
