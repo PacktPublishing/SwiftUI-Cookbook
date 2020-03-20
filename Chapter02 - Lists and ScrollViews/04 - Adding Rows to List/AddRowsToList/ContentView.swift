649a3ad6d2d95ce06d8b9d57aac418845408ab36
dedgar1 <jnzokwe@gmail.com>
2020-03-20 04:37:11 -0400

    Chapter 2, recipe 4

---
+++ Chapter02 - Lists and ScrollViews/04 - Adding Rows to List/AddRowsToList/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  AddRowsToList
+//
+//  Created by Edgar Nzokwe on 3/19/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var numbers = [1,2,3,4]
+    var body: some View {
+        NavigationView{
+            List{
+                ForEach(self.numbers, id:\.self){ number in
+                    Text("\(number)")
+                }
+            }.navigationBarTitle("Number List", displayMode: .inline)
+                .navigationBarItems(trailing: Button(action: {
+                    self.addItemToRow()
+                }){
+                    Text("Add")
+                })
+        }
+    }
+    private func addItemToRow() {
+        self.numbers.append(Int.random(in: 0 ..< 100))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
