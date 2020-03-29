b0a2023e3819677fd1bf5d0625fcc70283892c89
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-03-29 15:56:09 +0100

    Chapter 8, recipe 1

---
+++ Chapter08 - Driving SwiftUI with data and Combine/StaticTodoApp/StaticTodoApp/ContentView.swift
@@ -0,0 +1,62 @@
+//
+//  ContentView.swift
+//  StaticTodoApp
+//
+//  Created by giordano scalzo on 29/03/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct Todo: Identifiable, Equatable {
+    let id = UUID()
+    let description: String
+    var done: Bool
+}
+
+private extension Array where Element == Todo {
+    mutating func toggleDone(to todo: Todo) {
+        guard let index = self.firstIndex(
+            where: { $0 == todo }
+            ) else { return }
+        
+        self[index].done.toggle()
+    }
+}
+struct ContentView: View {
+    @State
+    private var todos: [Todo] = [
+        .init(description: "review first chapter", done: false),
+        .init(description: "buy wine", done: false),
+        .init(description: "paint kitchen", done: false),
+        .init(description: "cut the grass", done: false),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(todos) { todo in
+                    HStack {
+                        Text(todo.description)
+                            .strikethrough(todo.done)
+                        Spacer()
+                        Image(systemName:
+                            todo.done ?
+                                "checkmark.square" :
+                            "square")
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        self.todos.toggleDone(to: todo)
+                    }
+                }
+            }.navigationBarTitle("TODOs")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
