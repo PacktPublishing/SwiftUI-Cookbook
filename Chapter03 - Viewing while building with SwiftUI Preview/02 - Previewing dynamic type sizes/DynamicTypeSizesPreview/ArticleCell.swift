1d54426221a7a85554d1a3f579506dcb39802a8d
dedgar1 <jnzokwe@gmail.com>
2020-03-29 09:13:16 -0400

    Chapter 3, recipe 2

---
+++ Chapter03 - Viewing while building with SwiftUI Preview/02 - Previewing dynamic type sizes/DynamicTypeSizesPreview/ArticleCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArticleCell.swift
+//  DynamicTypeSizesPreview
+//
+//  Created by Edgar Nzokwe on 3/29/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import Foundation
+
+struct ArticleCell {
+    var imageName:String
+    var title:String
+    var description:String
+}
+
+let sampleCell = ArticleCell(imageName: "reading", title: "Love reading", description: "Reading is essential to success")
---
+++ Chapter03 - Viewing while building with SwiftUI Preview/02 - Previewing dynamic type sizes/DynamicTypeSizesPreview/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  DynamicTypeSizesPreview
+//
+//  Created by Edgar Nzokwe on 3/29/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var articleCell: ArticleCell  = ArticleCell(imageName: "", title: "", description: "")
+    var body: some View {
+        HStack{
+            Image(articleCell.imageName)
+            .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width:150, height:100)
+                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            VStack {
+                Text(articleCell.title)
+                    .font(.title)
+                Text(articleCell.description)
+                .padding()
+            }
+                
+        }
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        Group {
+            ContentView(articleCell: sampleCell)
+            ContentView(articleCell: sampleCell)
+                .environment(\.sizeCategory, .extraSmall)
+            ContentView(articleCell: sampleCell)
+            .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
+        }
+    }
+}
