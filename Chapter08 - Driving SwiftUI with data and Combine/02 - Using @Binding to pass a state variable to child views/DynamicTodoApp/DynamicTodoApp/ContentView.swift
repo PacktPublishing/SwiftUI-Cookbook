04810029d756e1eb2fef23dcda162ab978e2e199
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-03-29 17:42:28 +0100

    Chapter 8, recipe 2

--- Chapter08 - Driving SwiftUI with data and Combine/02 - Using @Binding to pass a state variable to child views/DynamicTodoApp/DynamicTodoApp/ContentView.swift
+++ Chapter08 - Driving SwiftUI with data and Combine/02 - Using @Binding to pass a state variable to child views/DynamicTodoApp/DynamicTodoApp/ContentView.swift
@@ -8,9 +8,99 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct CustomTextFieldStyle: TextFieldStyle {
+    func _body(configuration: TextField<Self._Label>) -> some View {
+        configuration
+            .padding(.horizontal, 8)
+            .autocapitalization(.none)
+            .frame(height: 40)
+            .background(Color.white)
+            .cornerRadius(5)
+    }
+}
+struct InputTodoView: View {
+    @State
+    private var newTodoDescription: String = ""
+    
+    @Binding
+    var todos: [Todo]
+    
     var body: some View {
-        Text("Hello, World!")
+        HStack {
+            TextField("Todo", text: $newTodoDescription)
+                .textFieldStyle(CustomTextFieldStyle())
+            Spacer()
+            Button(action: {
+                guard !self.newTodoDescription.isEmpty else {
+                    return
+                }
+                self.todos.append(Todo(description: self.newTodoDescription, done: false))
+                self.newTodoDescription = ""
+            }) {
+                Text("Add")
+                    .padding(.horizontal, 16)
+                    .padding(.vertical, 8)
+                    .foregroundColor(.white)
+                    .background(Color.green)
+                    .cornerRadius(5)
+            }
+        }
+        .frame(height: 60)
+        .padding(.horizontal, 24)
+        .padding(.bottom, 30)
+        .background(Color.gray)
+    }
+}
+
+
+struct Todo: Identifiable, Equatable {
+    let id = UUID()
+    let description: String
+    var done: Bool
+}
+
+private extension Array where Element == Todo {
+    mutating func toggleDone(to todo: Todo) {
+        guard let index = self.firstIndex(
+            where: { $0 == todo }
+            ) else { return }
+        
+        self[index].done.toggle()
+    }
+}
+struct ContentView: View {
+    @State
+    private var todos: [Todo] = [
+        .init(description: "review first chapter", done: false),
+        .init(description: "buy wine", done: false),
+        .init(description: "paint kitchen", done: false),
+        .init(description: "cut the grass", done: false),
+    ]
+    
+    var body: some View {
+        NavigationView {
+            ZStack(alignment: .bottom) {
+                List {
+                    ForEach(todos) { todo in
+                        HStack {
+                            Text(todo.description)
+                                .strikethrough(todo.done)
+                            Spacer()
+                            Image(systemName:
+                                todo.done ?
+                                    "checkmark.square" :
+                                "square")
+                        }
+                        .contentShape(Rectangle())
+                        .onTapGesture {
+                            self.todos.toggleDone(to: todo)
+                        }
+                    }
+                }.navigationBarTitle("TODOs")
+                InputTodoView(todos: self.$todos)
+            }
+            .edgesIgnoringSafeArea(.bottom)
+        }
     }
 }
 
