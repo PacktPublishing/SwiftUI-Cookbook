cc0923393443fdbaa33a312809022ceb87bfcf52
dedgar1 <jnzokwe@gmail.com>
2020-03-30 05:24:01 -0400

    Chapter 3, recipe 4

---
+++ Chapter03 - Viewing while building with SwiftUI Preview/04 - Previewing Layout on Different Devices/PreviewOnDifferentDevices/ContentView.swift
@@ -0,0 +1,37 @@
+  //
+//  ContentView.swift
+//  PreviewOnDifferentDevices
+//
+//  Created by Edgar Nzokwe on 3/30/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+          VStack{
+              Image("friendship")
+              .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  Text("Importance of Friendsip").font(.title)
+                  Text("More info on friendship").padding()
+          }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+                .previewDevice("iPhone 11 Pro Max")
+                .previewDisplayName("Iphone 11 Pro Max")
+            ContentView()
+                .previewDevice("iPhone 8")
+                .previewDisplayName("iPhone 8")
+            ContentView()
+                .previewLayout(.fixed(width: 568, height: 320))
+            .previewDisplayName("Custom Size to infer landscape mode")
+        }
+    }
+}
