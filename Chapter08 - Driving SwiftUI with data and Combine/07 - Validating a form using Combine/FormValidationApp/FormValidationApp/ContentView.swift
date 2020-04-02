17dc2b43f54d87f0a7c7edc851b802f3363b09ab
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-04-02 17:24:43 +0100

    Added image

---
+++ Chapter08 - Driving SwiftUI with data and Combine/07 - Validating a form using Combine/FormValidationApp/FormValidationApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FormValidationApp
+//
+//  Created by giordano scalzo on 02/04/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
