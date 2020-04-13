6e3d03802752d8ace687c35d255ec81fd8bc9da5
dedgar1 <jnzokwe@gmail.com>
2020-04-12 22:49:28 -0400

    Chapter 3, remove unused view in recipe 6 and add resource file for recipe 4 and 6

--- Chapter03 - Viewing while building with SwiftUI Preview/06 - Using Mock data for previews/UsingMockDataForPreviews/ContentView.swift
+++ Chapter03 - Viewing while building with SwiftUI Preview/06 - Using Mock data for previews/UsingMockDataForPreviews/ContentView.swift
@@ -38,13 +38,13 @@ struct ContentView_Previews: PreviewProvider {
     }
     static var testInsects : [Insect]{
         guard let url = Bundle.main.url(forResource: "insectData", withExtension: "json"),
-        let data = try? Data(contentsOf: url)
+        
+            let data = try? Data(contentsOf: url)
             else{
                 return[]
         }
         let decoder  = JSONDecoder()
         let array = try?decoder.decode([Insect].self, from: data)
-        print("test")
         return array ??  [testInsect]
     }
 }
