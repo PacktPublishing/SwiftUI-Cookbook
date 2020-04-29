ea56a3c1dcdda089cebd0c016c24414c0f2931a3
dedgar1 <jnzokwe@gmail.com>
2020-04-29 04:49:11 -0400

    Chapter 04, recipe 02 improved styling

--- Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/ContentView.swift
+++ Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/ContentView.swift
@@ -16,6 +16,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LoginView()
     }
 }
--- Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/LoginView.swift
+++ Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/LoginView.swift
@@ -9,21 +9,31 @@
 import SwiftUI
 
 struct LoginView: View {
-    @State var username = ""
-    @State var password = ""
+    @State private var username = ""
+    @State private var password = ""
     var body: some View {
         VStack{
-            AppName()
-            UserImage()
-                .padding(.bottom,30)
+            Text("Amazing Games")
+                .fontWeight(.heavy)
+                .foregroundColor(.blue)
+                .font(.largeTitle)
+                .padding(.bottom, 30)
+            
+            Image(systemName: "person.circle")
+                .font(.system(size: 150))
+                .foregroundColor(.gray)
+                .padding(.bottom,40)
+            
             Group{
                 TextField("Username", text: $username)
                 SecureField("Password", text: $password)
-            }.padding()
-            .background(Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)))
-                .cornerRadius(10.0)
-                .padding()
-            
+            }
+            .padding()
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color.black, lineWidth: 2)
+            )
+
             Button(action: {
                 print("Submit clicked")
             }){
@@ -40,24 +50,5 @@ struct LoginView: View {
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
         LoginView()
-    }
-}
-
-struct AppName: View {
-    var body: some View {
-        Text("Amazing App")
-            .fontWeight(.heavy)
-            .foregroundColor(.blue)
-            .font(.largeTitle)
-            .padding(.bottom, 30)
-    }
-}
-
-struct UserImage: View {
-    var body: some View {
-        Image(systemName: "person.circle")
-            .font(.system(size: 150))
-            .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
-
     }
 }
