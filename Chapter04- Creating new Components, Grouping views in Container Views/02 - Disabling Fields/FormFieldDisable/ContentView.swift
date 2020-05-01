4ac312bccdfcd3cb9e5c87fa89ffa961192ac794
dedgar1 <jnzokwe@gmail.com>
2020-04-30 23:41:46 -0400

    chapter 04, recipe 02, display loginview from contentview

--- Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/ContentView.swift
+++ Chapter04- Creating new Components, Grouping views in Container Views/02 - Disabling Fields/FormFieldDisable/ContentView.swift
@@ -10,12 +10,12 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        LoginView()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        LoginView()
+        ContentView()
     }
 }
