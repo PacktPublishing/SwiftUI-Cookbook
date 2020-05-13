1af20fc1a6b47b3192faeea141914ebb2f23a98a
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-05-13 23:05:30 +0100

    C11 r2

---
+++ Chapter11 - Handling Core Data and SwiftUI/02 - Showing Core Data objects with @FetchRequest/FetchContactsApp/FetchContactsApp/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  FetchContactsApp
+//
+//  Created by giordano scalzo on 13/05/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @FetchRequest(
+        entity: Contact.entity(),
+        sortDescriptors: [
+            NSSortDescriptor(keyPath: \Contact.lastName, ascending: true),
+            NSSortDescriptor(keyPath: \Contact.firstName, ascending: true),
+        ]
+    )
+    var contacts: FetchedResults<Contact>
+
+    var body: some View {
+        List(contacts, id: \.self) { contact in
+            Text(contact.firstName ?? "-")
+            Text(contact.lastName ?? "-")
+            Spacer()
+            Text(contact.phoneNumber ?? "-")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
