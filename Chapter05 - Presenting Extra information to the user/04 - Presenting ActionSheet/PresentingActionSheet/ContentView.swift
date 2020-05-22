122f31d3c4d4b5cba6ba628f88a1fa3e367b7b1d
dedgar1 <jnzokwe@gmail.com>
2020-05-21 22:46:02 -0400

    Chapter 05 - recipe 04

---
+++ Chapter05 - Presenting Extra information to the user/04 - Presenting ActionSheet/PresentingActionSheet/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  PresentingActionSheet
+//
+//  Created by Edgar Nzokwe on 5/21/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showSheet = false
+    var body: some View {
+        Text("Present Sheet")
+            .onTapGesture {
+                self.showSheet = true
+        }
+        .actionSheet(isPresented: $showSheet){
+            ActionSheet(
+                title: Text("ActionSheet"),
+                message: Text("Use Action sheet to give users alteratives to completing a task").font(.largeTitle),
+                buttons: [
+                    .default(Text("Dismiss Sheet")),
+                    .default(Text("Save")),
+                    .destructive(Text("Cancel")),
+                    .default(Text("Print to console"), action: {
+                        print("Print button clicked")
+                    })
+            ])
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
