96a1ee16ae0511e86490e777d0df485f52d24b6b
dedgar1 <jnzokwe@gmail.com>
2020-05-31 11:25:59 -0400

    add section to read from digital crown

---
+++ Chapter12 - Cross-Platform SwiftUI/01 - Reading The digital Crown/watchOSDigitalCrown WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  watchOSDigitalCrown WatchKit Extension
+//
+//  Created by Edgar Nzokwe on 5/31/20.
+//  Copyright © 2020 Edgar Nzokwe. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
