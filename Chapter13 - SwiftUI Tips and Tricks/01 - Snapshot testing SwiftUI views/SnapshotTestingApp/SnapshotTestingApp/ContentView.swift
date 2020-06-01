d9ef67cf8a6d460224637917e3179340cd52cba2
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-06-01 19:21:14 +0100

    Chapter 13 r1d

---
+++ Chapter13 - SwiftUI Tips and Tricks/01 - Snapshot testing SwiftUI views/SnapshotTestingApp/SnapshotTestingApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnapshotTestingApp
+//
+//  Created by giordano scalzo on 01/06/2020.
+//  Copyright © 2020 giordano scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
