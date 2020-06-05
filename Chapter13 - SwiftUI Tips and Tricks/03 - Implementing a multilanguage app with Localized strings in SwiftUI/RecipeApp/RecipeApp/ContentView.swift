50aff3bd72f49f22539a88f2dffcc893779ff13f
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-06-05 22:38:01 +0100

    Ch13 r3

--- Chapter13 - SwiftUI Tips and Tricks/03 - Implementing a multilanguage app with Localized strings in SwiftUI/RecipeApp/RecipeApp/ContentView.swift
+++ Chapter13 - SwiftUI Tips and Tricks/03 - Implementing a multilanguage app with Localized strings in SwiftUI/RecipeApp/RecipeApp/ContentView.swift
@@ -8,14 +8,95 @@
 
 import SwiftUI
 
+struct Timings {
+    let cooking: Int
+    let baking: Int
+    let resting: Int
+}
+
 struct ContentView: View {
+    private let timings = Timings(cooking: 20,
+                                baking: 5,
+                                resting: 10)
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            CoverView()
+            Divider()
+            TimingsView(timings: timings)
+            Divider()
+            IngredientsView()
+            Spacer()
+        }
+    }
+}
+
+struct IngredientsView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text("recipe.ingredients.title")
+                .font(.system(size: 28))
+                .fontWeight(.bold)
+            Text("recipe.ingredients.servings")
+                .fontWeight(.bold)
+            Text("recipe.ingredients.ingredient1")
+            Text("recipe.ingredients.ingredient2")
+            Text("recipe.ingredients.ingredient3")
+            Text("recipe.ingredients.ingredient4")
+            Text("recipe.ingredients.ingredient5")
+
+        }
+    }
+}
+
+struct TimingsView: View {
+    let timings: Timings
+
+    var body: some View {
+        HStack(spacing: 64) {
+            VStack {
+                Text("recipe.timings.cooking.title")
+                    .fontWeight(.bold)
+                Text("recipe.timings.cooking.value \(timings.cooking)")
+            }
+            VStack {
+                Text("recipe.timings.baking.title")
+                    .fontWeight(.bold)
+                Text("recipe.timings.baking.value \(timings.baking)")
+            }
+            VStack {
+                Text("recipe.timings.resting.title")
+                    .fontWeight(.bold)
+                Text("recipe.timings.resting.value \(timings.resting)")
+            }
+        }
+    }
+}
+
+struct CoverView: View {
+    var body: some View {
+        VStack {
+            Text("recipe.cover.name")
+                .font(.largeTitle)
+            Image("recipe")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            HStack {
+                Text("recipe.cover.difficulty.title")
+                    .font(.system(size: 28))
+                    .fontWeight(.bold)
+                Text("recipe.cover.difficulty.value")
+                    .font(.system(size: 28))
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ForEach(["en", "it", "fr"], id: \.self) { l in
+            ContentView()
+                .environment(\.locale, .init(identifier: l))
+        }
     }
 }
