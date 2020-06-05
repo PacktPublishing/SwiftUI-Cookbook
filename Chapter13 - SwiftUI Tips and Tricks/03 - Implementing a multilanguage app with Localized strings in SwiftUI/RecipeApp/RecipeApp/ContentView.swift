ccf7f2674deefa64d366953d5645a01f99a5c1e1
Giordano Scalzo <giordano.scalzo@gmail.com>
2020-06-05 18:54:49 +0100

    Chapter13 r3 wip

---
+++ Chapter13 - SwiftUI Tips and Tricks/03 - Implementing a multilanguage app with Localized strings in SwiftUI/RecipeApp/RecipeApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecipeApp
+//
+//  Created by giordano scalzo on 05/06/2020.
+//  Copyright © 2020 Giordano Scalzo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
