fc3e377d7beef994b4c36147a1fce72202ba90ae
dedgar1 <jnzokwe@gmail.com>
2020-09-20 11:32:37 -0400

    initial code for disclosuregroup

---
+++ Chapter02 - Lists and ScrollViews/12 - DisclosureGroup/UsingDisclosureGroup/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  UsingDisclosureGroup
+//
+//  Created by Edgar Nzokwe on 9/20/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var showplanets = true
+    var body: some View {
+        VStack {
+            DisclosureGroup("Planets", isExpanded: $showplanets){
+                Text("Mercury")
+                Text("Venus")
+                
+                DisclosureGroup("Earth"){
+                    Text("North America")
+                    Text("South America")
+                    Text("Europe")
+                    Text("Africa")
+                    Text("Asia")
+                    Text("Antarctica")
+                    Text("Oceania")
+                }
+            }
+            DisclosureGroup{
+                Text("Surprise! This is an alternative way of using DisclosureGroup")
+            } label : {
+                Label("Tap to reveal", systemImage: "cube.box")
+                    .font(.system(size:25, design: .rounded))
+                    .foregroundColor(.blue)
+            }
+        }.padding()
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Chapter02 - Lists and ScrollViews/12 - DisclosureGroup/UsingDisclosureGroup/UsingDisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingDisclosureGroupApp.swift
+//  UsingDisclosureGroup
+//
+//  Created by Edgar Nzokwe on 9/20/20.
+//
+
+import SwiftUI
+
+@main
+struct UsingDisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
