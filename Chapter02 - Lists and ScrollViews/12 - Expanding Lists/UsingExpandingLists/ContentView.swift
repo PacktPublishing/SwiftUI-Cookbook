e9260f7dc74613db6f9deb4995818d4d8c9d1493
dedgar1 <jnzokwe@gmail.com>
2020-10-01 03:30:12 -0400

    fix spelling in Chapter 2 expanding list

--- Chapter02 - Lists and ScrollViews/12 - Expanding Lists/UsingExpandingLists/ContentView.swift
+++ Chapter02 - Lists and ScrollViews/12 - Expanding Lists/UsingExpandingLists/ContentView.swift
@@ -23,19 +23,19 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-struct Bagpack: Identifiable {
+struct Backpack: Identifiable {
     let id = UUID()
     let name: String
     let icon: String
-    var content: [Bagpack]?
+    var content: [Backpack]?
 }
-let dollar = Bagpack(name: "Dollar", icon: "dollarsign.circle")
-let yen = Bagpack(name: "Yen",icon: "yensign.circle")
-let currencies = Bagpack(name: "Currencies", icon: "coloncurrencysign.circle", content: [dollar, yen])
+let dollar = Backpack(name: "Dollar", icon: "dollarsign.circle")
+let yen = Backpack(name: "Yen",icon: "yensign.circle")
+let currencies = Backpack(name: "Currencies", icon: "coloncurrencysign.circle", content: [dollar, yen])
 
-let pencil = Bagpack(name: "Pencil",icon: "pencil.circle")
-let hammer = Bagpack(name: "Hammer",icon: "hammer")
-let paperClip = Bagpack(name: "Paperclip",icon: "paperclip")
-let glass = Bagpack(name: "Magnifying glass", icon: "magnifyingglass")
-let bin  = Bagpack(name: "Bin", icon: "arrow.up.bin", content: [paperClip, glass])
-let tools = Bagpack(name: "Tools", icon: "folder", content: [pencil, hammer,bin])
+let pencil = Backpack(name: "Pencil",icon: "pencil.circle")
+let hammer = Backpack(name: "Hammer",icon: "hammer")
+let paperClip = Backpack(name: "Paperclip",icon: "paperclip")
+let glass = Backpack(name: "Magnifying glass", icon: "magnifyingglass")
+let bin  = Backpack(name: "Bin", icon: "arrow.up.bin", content: [paperClip, glass])
+let tools = Backpack(name: "Tools", icon: "folder", content: [pencil, hammer,bin])
